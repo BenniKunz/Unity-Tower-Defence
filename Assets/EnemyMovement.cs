30808cfc93bbe13df0db5097320aa6ed9c513d4f
LAPTOP-J67JR8TN\benni <bennikunzweb@web.de>
2020-12-01 11:16:40 +0100

    Enemy Path

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] 
+    [SerializeField] List<Waypoint> path;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(FindPath());
+    }
+
+    IEnumerator FindPath()
+    {
+        print("Starting patrol");
+        foreach(Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            print($"Visiting block {waypoint.name}");
+        }
+        print("Ending patrol");
+        yield return new WaitForSeconds(1f);
     }
 
     // Update is called once per frame
@@ -17,4 +29,5 @@ public class EnemyMovement : MonoBehaviour
     {
         
     }
+
 }
