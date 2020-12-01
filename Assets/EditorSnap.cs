71b22e32925118dc12923d33d6b06572eb267819
LAPTOP-J67JR8TN\benni <bennikunzweb@web.de>
2020-12-01 08:23:49 +0100

    snap tool edit mode

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [Range(1f, 20f)]
+    [SerializeField] int gridSize;
+    
+
+    void Update()
+    {
+        Vector3 snapPos;
+
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+}
