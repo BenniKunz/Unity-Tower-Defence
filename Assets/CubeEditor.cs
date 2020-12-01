c55477d163242b8f1b45e7474697a290d6df3487
LAPTOP-J67JR8TN\benni <bennikunzweb@web.de>
2020-12-01 09:29:34 +0100

    create path

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,21 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     // Start is called before the first frame update
 
     [Range(1f, 20f)]
-    [SerializeField] int gridSize;
-    
+    [SerializeField] float gridSize = 10f;
+
+    TextMesh textMesh;
+
+    private void Start()
+    {
+        
+    }
 
     void Update()
     {
         Vector3 snapPos;
-
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        textMesh.text = labelText;
+        gameObject.name = labelText;
+
     }
 }
