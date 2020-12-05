5c2eb598eca49cf2fc269894ac5e76c4b31c76a6
LAPTOP-J67JR8TN\benni <bennikunzweb@web.de>
2020-12-05 16:39:36 +0100

    new waypoints, assets

---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+[RequireComponent (typeof(Waypoint))]
+public class CubeEditor : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Waypoint waypoint;
+
+
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
+
+    void Update()
+    {
+        SnapToGrid();
+        UpdateLabel();
+    }
+
+    private void SnapToGrid()
+    {
+        int gridSize = waypoint.GetGridSize();
+        Vector2Int gridPos = waypoint.CalcGridPos() * gridSize;
+        transform.position = new Vector3(gridPos.x, 0f, gridPos.y);
+    }
+
+    private void UpdateLabel()
+    {
+        Vector2Int gridPos = waypoint.CalcGridPos();
+
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = gridPos.x + "," + gridPos.y;
+        textMesh.text = labelText;
+        gameObject.name = labelText;
+    }
+}
---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    [SerializeField] Waypoint startWaypoint, endWaypoint;
+
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Queue<Waypoint> queue = new Queue<Waypoint>();
+
+    bool isRunning = true;
+    Waypoint searchCenter;
+    List<Waypoint> path = new List<Waypoint>();
+
+
+    Vector2Int[] directions = { Vector2Int.up, Vector2Int.right, Vector2Int.down, Vector2Int.left };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public List<Waypoint> GetPath()
+    {
+        LoadBlocks();
+        ColorStartEnd();
+        BreadthFirstSearch();
+        CreatePath();
+        return path;
+    }
+
+    private void CreatePath()
+    {
+        path.Add(endWaypoint);
+        Waypoint previous = endWaypoint.exploredFrom;
+
+        while(previous != startWaypoint)
+        {
+            path.Add(previous);
+            previous = previous.exploredFrom;
+        }
+        path.Add(startWaypoint);
+        path.Reverse();
+    }
+
+    private void BreadthFirstSearch()
+    {
+        queue.Enqueue(startWaypoint);
+        while (queue.Count > 0 && isRunning)
+        {
+            searchCenter = queue.Dequeue();
+
+            StopIfEndFound();
+            ExploreNeighbours();
+            searchCenter.isExplored = true;
+        }
+    }
+
+    private void StopIfEndFound()
+    {
+        if (searchCenter == endWaypoint)
+        {
+            print("Attention! Start = End");
+            isRunning = false;
+        }
+    }
+
+    private void ExploreNeighbours()
+    {
+        if (!isRunning)
+        {
+            return;
+        }
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int explorationCoordinates = searchCenter.CalcGridPos() + direction;
+
+            if (grid.ContainsKey(explorationCoordinates))
+            {
+                QueueNewNeighbours(explorationCoordinates);
+            }
+        }
+    }
+
+    private void QueueNewNeighbours(Vector2Int neighbourCoordinates)
+    {
+        Waypoint neighbour = grid[neighbourCoordinates];
+        if (neighbour.isExplored == false && !(queue.Contains(neighbour)))
+        {
+            //neighbour.SetTopColor(Color.blue);
+            queue.Enqueue(neighbour);
+            neighbour.exploredFrom = searchCenter;
+        }
+    }
+
+    private void ColorStartEnd()
+    {
+        startWaypoint.SetTopColor(Color.green);
+        endWaypoint.SetTopColor(Color.black);
+    }
+
+    private void LoadBlocks()
+    {
+        Waypoint[] waypoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in waypoints)
+        {
+            if (!(grid.ContainsKey(waypoint.CalcGridPos())))
+            {
+                grid.Add(waypoint.CalcGridPos(), waypoint);
+            }
+            else
+            {
+                Debug.LogWarning("Overlapping block:" + waypoint.name);
+            }
+        }
+    }
+
+}
