cdff2a534e3c764f52679359d2534a00c88b4776
LAPTOP-J67JR8TN\benni <bennikunzweb@web.de>
2020-12-06 08:27:11 +0100

    Add Robo, add Towers, Towers Particle System

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+
+    // Update is called once per frame
+    void Update()
+    {
+        FocusOnEnemy();
+    }
+
+    private void FocusOnEnemy()
+    {
+        objectToPan.LookAt(targetEnemy);
+    }
+}
