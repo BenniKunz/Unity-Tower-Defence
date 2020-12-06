d7e24bdb7f3ea113a03efdd737ad08b724c6534c
LAPTOP-J67JR8TN\benni <bennikunzweb@web.de>
2020-12-06 09:58:45 +0100

    random

---
+++ Assets/EnemyCollision.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour
+{
+
+    [SerializeField] int scorePerHit = 1;
+    [SerializeField] int hits = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    
+
+    private void OnParticleCollision(GameObject other)
+    {
+        hits--;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -19,4 +19,6 @@ public class Tower : MonoBehaviour
     {
         objectToPan.LookAt(targetEnemy);
     }
+
+
 }
