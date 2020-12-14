88d3e0dc578deb62c4445a0e51b4d841ec070475
LAPTOP-J67JR8TN\benni <bennikunzweb@web.de>
2020-12-14 10:54:21 +0100

    Smooth Movement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,30 +4,33 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    public float speed = 1f;
+    Waypoint waypoint;
+    int waypointPosition = 0;
     // Start is called before the first frame update
     void Start()
     {
         Pathfinder pathfinder = FindObjectOfType<Pathfinder>();
         List<Waypoint> path = pathfinder.GetPath();
-        StartCoroutine(FollowPath(path));
-    }
-
-    IEnumerator FollowPath(List <Waypoint> path)
-    {
-        
-        foreach (Waypoint waypoint in path)
-        {
-            transform.position = waypoint.transform.position;
-
-            yield return new WaitForSeconds(1f);
-        }
-        
+        transform.position = path[0].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        Pathfinder pathfinder = FindObjectOfType<Pathfinder>();
+        List<Waypoint> path = pathfinder.GetPath();
+        waypoint = path[waypointPosition];
 
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, waypoint.transform.position, step);
+        float distance = Vector3.Distance(transform.position, waypoint.transform.position);
+
+        if(distance < 0.001f)
+        {
+            if(waypointPosition < path.Count -1)
+            waypointPosition++;
+        }
+    }
 }
