a7d26d31d310f8b1af6327770df423e05e97001e
LAPTOP-J67JR8TN\benni <bennikunzweb@web.de>
2020-12-15 20:39:29 +0100

    Canvas Text for Enemies and Health

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,25 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int healthPoints = 3;
     [SerializeField] int healtDecrease = 1;
+    [SerializeField] Text healthText;
+    [SerializeField] Text EnemieCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthText.text = healthPoints.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        EnemyCollision[] enemies = FindObjectsOfType<EnemyCollision>();
+        int numEnemies = enemies.Length;
+
+        EnemieCount.text = numEnemies.ToString();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        healthPoints = healthPoints - healtDecrease;
+        healthPoints -= healtDecrease;
+        healthText.text = healthPoints.ToString();
     }
 }
